e23b89a864b4b6fe6e23fb3265c94b83d911da90
Sergey Minakov <s.minakov@be-interactive.ru>
2016-10-20 12:12:18 +0300

    change package

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/naithar/Kitura-net.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 14),
         .Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 1, minor: 0)
     ],
